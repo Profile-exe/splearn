c8ac2d3abf44df27319a4f4a0870941b5b4cdf21
Profile-exe <kang67572346@gmail.com>
2025-08-19 22:21:26 +0900

    feat: PasswordEncoder 도메인 서비스 구현 #10

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.security:spring-security-core")
     implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.12.0")
 
     compileOnly("org.projectlombok:lombok")
