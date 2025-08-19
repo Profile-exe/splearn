5366239e212f55181d2d7649efeed97c9b27bd43
Profile-exe <kang67572346@gmail.com>
2025-08-20 03:43:38 +0900

    feat: Profile 값 객체 구현 및 테스트 #12

---
+++ src/test/java/tobyspring/splearn/domain/member/ProfileTest.java
@@ -0,0 +1,35 @@
+package tobyspring.splearn.domain.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class ProfileTest {
+    @Test
+    void profile() {
+        assertThatNoException().isThrownBy(() -> new Profile("dding"));
+        assertThatNoException().isThrownBy(() -> new Profile("12345"));
+        assertThatNoException().isThrownBy(() -> new Profile("test2025"));
+    }
+
+    @Test
+    void profileFail() {
+        assertThatThrownBy(() -> new Profile(""))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Profile("toolongtoolongtoolong"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Profile("A"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Profile("프로필"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void url() {
+        var profile = new Profile("dding");
+
+        assertThat(profile.url()).isEqualTo("@dding");
+    }
+}
